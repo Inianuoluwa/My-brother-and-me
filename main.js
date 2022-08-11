03c06fe704ba98522398bb7a8e31f2a0769565dc
Inianuoluwa <110983838+Inianuoluwa@users.noreply.github.com>
2022-08-11 15:59:55 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
 alert('Welcome to this website.');
 alert('I hope you enjoy it.');
+let personality = 'new'
+if (personality = 'new'){
+  alert('You are new to this page.')
+}else {
+  alert('You are not new to this page.')
+}
